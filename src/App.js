b3b78c3c7a96e2326611a8f177ad8138f70e2187
tuanna2122 <tuanna2122@gmail.com>
2021-05-13 23:50:15 +0700

    setup basic react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Users from './containers/Users';
+import asyncComponent from './hoc/asyncComponent';
+
+const AsyncPizza = asyncComponent(() => import('./containers/Pizza'));
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Link to="/"></Link>
+                    &nbsp;|&nbsp;
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" exact component={AsyncPizza} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
